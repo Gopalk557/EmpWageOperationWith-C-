d01f4c6b0c4876cd91a30270482821e585700f5d
GOPAL THAKUR <gopalk557@gmail.com>
2022-02-11 15:07:33 +0530

    [Gopal]add:Employee part Time Sal

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace EmpWageOperation
     {
         static void Main(string[] args)
         {
-            EmpWagePrograms.DailyWage();
+            EmpWagePrograms.PartTimeEmpWage();
             Console.ReadKey();
         }
     }
