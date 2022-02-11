a6bb6775df0b063ab3d12e91c98b77821d927cfc
GOPAL THAKUR <gopalk557@gmail.com>
2022-02-11 14:35:24 +0530

    WELCOME TO EMPLOYEE WAGE PROGRAM

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Program");
+            Console.ReadKey();
+        }
+    }
+}
