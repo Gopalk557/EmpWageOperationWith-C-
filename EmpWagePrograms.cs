d9667ac7932907d26d65355447b215b6b60cede9
GOPAL THAKUR <gopalk557@gmail.com>
2022-02-11 15:17:54 +0530

    [Gopal]Add:Get Employee Wage Using Switch Case

--- EmpWagePrograms.cs
+++ EmpWagePrograms.cs
@@ -8,27 +8,36 @@ namespace EmpWageOperation
 {
     internal class EmpWagePrograms
     {
-        public static void PartTimeEmpWage()
+        //const, readonly
+        public const int IS_PART_TIME = 1;
+        //public readonly int Test=12;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public static void EmpWageUsingSwitch()
         {
             //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
+
             //variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
-                empHrs = 4;
-            else if (empCheck == IS_FULL_TIME)
-                empHrs = 8;
-            else
-                empHrs = 0;
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage: " + empWage);
-            Console.ReadKey();
         }
+
     }
 }
 
